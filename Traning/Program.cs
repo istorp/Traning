3a11f08a1ae547b874d3033981b27c27529558b9
Anders Djerf <37343667+istorp@users.noreply.github.com>
2020-03-30 23:58:06 +0200

    Add project files.

---
+++ Traning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Traning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
